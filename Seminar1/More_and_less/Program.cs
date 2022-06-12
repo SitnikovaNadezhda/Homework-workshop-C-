276c917b800f5e463c8474ace8ea0224b7a5e73f
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-06-12 21:38:31 +0300

    More and less

---
+++ Seminar1/More_and_less/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+int min = numberB;
+
+if (numberA < min) min = numberA;
+   Console.Write("Меньшее число: ");
+   Console.WriteLine(min);
+
+if (numberB > max) max = numberB;
+   Console.Write("Большее число: ");
+   Console.WriteLine(max);
+
