fc387bca1bdc161a9012b0a7baecdceb01a799b7
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-06-13 23:35:19 +0300

    is even

---
+++ Seminar1/Is_even/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0){
+    Console.WriteLine("Число четное");
+}
+else {
+    Console.WriteLine("Число нечетное");
+}
