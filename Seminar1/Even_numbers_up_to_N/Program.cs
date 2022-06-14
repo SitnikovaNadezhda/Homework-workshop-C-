3acd227cfb5270ebd5ecaea66f19d0b26c1cd982
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-06-14 15:08:55 +0300

    even numbers up to n

---
+++ Seminar1/Even_numbers_up_to_N/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = 0;
+if (num % 2 == 1){
+    num = num -1; 
+}
+
+while (num2 < num ){
+   num2 = num2 + 2;
+   Console.Write(num2 + " ");
+}
+
+
+
+
+
+
