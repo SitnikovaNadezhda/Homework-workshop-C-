1360fc8cfc5133e8f71d0d9efe5a4c6f0da78ff9
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-06-16 13:30:14 +0300

    daus of the week

---
+++ Seminar2/Daus_of_the_ week/Program.cs
@@ -0,0 +1,20 @@
+﻿void DausOfTheWeek(int a){
+    if(a <= 5){
+        Console.WriteLine("Будний день!");
+    }
+    else{
+        Console.WriteLine("Выходной день!");
+    }
+
+}
+Console.Write("Введите число: ");
+int num =Convert.ToInt32(Console.ReadLine());
+{
+if (num > 7){
+    Console.WriteLine(" В неделе только 7 дней!");
+}
+else{
+    DausOfTheWeek(Convert.ToInt32(Console.ReadLine()));
+
+}
+}
