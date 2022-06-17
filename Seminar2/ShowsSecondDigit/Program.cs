0f9f1276dd0d24c05c07db472d8c886d8b0b82b1
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-06-17 23:30:22 +0300

    Добавления ограничения диапозона чисел

--- Seminar2/ShowsSecondDigit/Program.cs
+++ Seminar2/ShowsSecondDigit/Program.cs
@@ -2,11 +2,16 @@
 int number = num / 10 % 10;
   return number;
 }
-Console.WriteLine("Введите трех значное число ");
+Console.WriteLine("Введите трехзначное число ");
 int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 | num > 999){
+    Console.WriteLine("Введено некорректное число");
+}
+else{
 int a = SecondDigit(num);
 Console.WriteLine(a);
-
+}
 
 
 
--- Seminar2/ShowsThirdDigit/Program.cs
+++ Seminar2/ShowsThirdDigit/Program.cs
@@ -1,6 +1,18 @@
-﻿Console.Write("Введите трех знычное число: ");
+﻿int ThirdDigit(int num){
+    int a = 0;
+    a = num % 10;
+  return a;
+}
+
+
+Console.Write("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num < 100 && num > 999){
-    Console.WriteLine("Введено некорректное число!");
+if (num < 100 | num > 999){
+    Console.WriteLine("Введено некорректное число");
 }
+else{
+int a = ThirdDigit(num);
+Console.WriteLine(a);
+}
+
