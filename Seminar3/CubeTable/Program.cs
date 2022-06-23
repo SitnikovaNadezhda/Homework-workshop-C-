1e612c64a98edbe4a0895176ca6e3080985b3215
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-06-23 16:16:01 +0300

    cube table

---
+++ Seminar3/CubeTable/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+void listSqrt(int n)
+{
+int count = 1;
+
+Console.Write(n + "-> ");
+
+if (n > 0)
+{
+
+while (count <= n)
+{
+Console.Write(Math.Pow(count, 3) + ", ");
+count++;
+}
+}
+else
+{
+count = -1;
+while (count >= n)
+{
+Console.Write(Math.Pow(count, 3) + ", ");
+count--;
+}
+}
+}
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+listSqrt(num);
