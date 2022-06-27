675794bf90b7e6f33d6be9561907ec4a79af452f
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-06-27 14:18:18 +0300

    sum of numbers

---
+++ Seminar4/SumOfNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumOfNumbers( int a){
+    int numA = 0;
+    int numB = a;
+    int sum = 0;
+   while(numB >0){
+    numA = numB % 10;
+    sum = sum + numA;
+    numB = numB / 10;
+   }
+   return sum;
+}
+
+
+Console.WriteLine("Enter the number:");
+int num = Convert.ToInt32(Console.ReadLine());
+int sunnum=SumOfNumbers(num);
+Console.WriteLine(sunnum);
+
