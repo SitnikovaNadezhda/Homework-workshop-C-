7945c77a2d6f3f8cd9aa68d49131e6fd5f684f32
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-06-27 13:34:13 +0300

    array of 8 elements

--- Seminar4/ArrayOf8Elements/Program.cs
+++ Seminar4/ArrayOf8Elements/Program.cs
@@ -1,16 +1,31 @@
-﻿
-int[] CreateRandomArray(int size, int min, int max){
-    int[] array = new int[size];
-    for(int i = 0; i < size; i++){
-        array[i] = new Random().Next(min,max+1);
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int [] CreateRandomArrey( int size)
+{
+    int[] array = new int [size];
+    for (int i =0; i < size ; i++){
+        array[i]=new Random().Next(0,100);
     }
     return array;
 }
 
-void ShowArray (int[] array){
-
-    for (int i = 0; i < array.Length; i++){
-        Console.Write(array[i] + " ");
+int [] CreateArray(int size)
+{
+    int[] array = new int [size];
+    for(int i = 0; i < size; i++){
+        Console.Write("Input" + i + "element: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
     }
+    return array;
 }
 
+void ShowArray( int[] array)
+{
+    for (int i = 0; i < array.Length; i++){
+        Console.Write( array[i]+" ");
+    }
+}
+//  int[] myRandomArray = CreateRandomArrey(8);
+//  ShowArray(myRandomArray);
+
+int [] secondarray = CreateArray(8);
+ShowArray(secondarray);
