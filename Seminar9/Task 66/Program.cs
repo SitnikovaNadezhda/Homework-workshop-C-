cd41323a5625873c607e00207bed4d4e4db6a6f3
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-07-15 14:42:45 +0300

    problem solving

---
+++ Seminar9/Task 66/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+Console.WriteLine("Введите первое число:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{m},{n},{SumNumbers(m)}");
+int SumNumbers(int m, int n)
+{
+    // if (m> n){
+    
+    // }
+    if(m==n) return m;
+    return (m + SumNumbers(m+1,n));
+   
+}
+Console.WriteLine($"{m},{n},{SumNumbers(m,n)}");
+
---
+++ Seminar9/Task 68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.WriteLine("Задайте число элементу 'n':");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте число элементу 'm':");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int ackermansAnswer = AckermanFunc(n, m);
+Console.WriteLine($"Результат функции Аккермана: {ackermansAnswer}");
+int AckermanFunc(int n, int m)
+{
+    if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
+    if (n == 0) return m + 1;
+    if (m == 0) return AckermanFunc(n - 1, m);
+    return AckermanFunc(n - 1, AckermanFunc(n, m - 1));
+}
+
+
+
+
+
+
